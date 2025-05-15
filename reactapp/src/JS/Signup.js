84798fdbaa12c845af73eca851646d8f2721bf38
helauren <helauren42@gmail.com>
2025-05-15 21:22:09 +0200

    sending post request on signup

--- reactapp/src/JS/Signup.js
+++ reactapp/src/JS/Signup.js
@@ -1,14 +1,35 @@
 import { useEffect, useState } from "react";
 import { Link } from 'react-router-dom'
+import { SOCKET_ADDRESS } from "./Const";
 
 export const SignupPage = () => {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("")
   const inputChange = (e) => {
     return e.target.value
   }
-  const submitSignup = (e) => {
-    console.log("submitting signup", e)
+  const submitSignup = async (e) => {
+    e.preventDefault()
+    console.log("submitting login: ", e)
+    console.log(username)
+    console.log(password)
+    console.log(email)
+    const endpoint = `${SOCKET_ADDRESS}/signup`
+    const body = JSON.stringify({ username, password, email })
+    const data = await fetch(endpoint,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: 'include',
+        body: body
+      }
+    )
+    console.log(endpoint)
+    setUsername("")
+    setPassword("")
+    setEmail("")
+    e.target.reset()
   }
   return (
     <div className="account-base-container">
@@ -22,6 +43,10 @@ export const SignupPage = () => {
           <div className='input-block'>
             <h2 className='input-header'>password</h2>
             <input className='input-input' required type="text" onChange={(e) => setPassword(inputChange(e))} />
+          </div>
+          <div className='input-block'>
+            <h2 className='input-header'>email</h2>
+            <input className='input-input' required type="text" onChange={(e) => setEmail(inputChange(e))} />
           </div>
           <div className='centerx-container'>
             <button className='account-submit-btn'>submit</button>
