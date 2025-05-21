0b4b0aaec41b2d28db88f505451d8b002c82f29a
helauren <helauren42@gmail.com>
2025-05-21 12:04:03 +0200

    sign out working

--- reactapp/src/JS/Account.js
+++ reactapp/src/JS/Account.js
@@ -1,7 +1,17 @@
+import { useState, useEffect, useContext } from 'react';
+import { AppContext } from "./App.js"
+import { Link, useNavigate } from 'react-router-dom'
+
 export const AccountPage = () => {
+  const [signedIn, setSignedIn] = useContext(AppContext)
   return (
     <div>
-      <h1>Account Page</h1>
+      <Link to={"/login"}>
+        <h1>Account Page</h1>
+        <button onClick={() => {
+          setSignedIn(false)
+        }}>Log out </button>
+      </Link>
     </div>
   )
 }
