dd4e3929ef07f43889ca67eae3a688169cf6dcee
helauren42 <helauren42@gmail.com>
2025-06-25 13:54:26 +0200

    home page and formatter did a bunch more stuff

--- reactapp/src/JS/Home.jsx
+++ reactapp/src/JS/Home.jsx
@@ -1,12 +1,33 @@
 import { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { AppContext } from "./App.jsx"
+import '../CSS/Home.css'
+
+const SigninSection = ({ navigate }) => {
+	return (
+		<section id="signin-section">
+			<button className="home-btn" onClick={() => navigate("/login")} ><p>Sign in</p></button>
+		</section>
+	)
+}
+
+const PlayGame = ({ navigate }) => {
+	return (
+		<section id="playgame-section">
+			<button className="home-btn" onClick={() => navigate("/play/matchmaking")}><p>Play now</p></button>
+		</section>
+	)
+}
 
 export const HomePage = () => {
 	const [signedIn, setSignedIn] = useContext(AppContext)
+	const navigate = useNavigate()
 	return (
-		<>
-			<h1>You are {signedIn ? "signed in" : "not signed in"}</h1>
-		</>
+		<section id="home-page">
+			<h1 id="home-title" >Welcome to the best online chess platform</h1>
+			<h2 id="home-subtitle">Play against other top chess players and chat with them</h2>
+			{signedIn ? <PlayGame navigate={navigate} /> : <SigninSection navigate={navigate} />}
+		</section>
 	)
 }
 
